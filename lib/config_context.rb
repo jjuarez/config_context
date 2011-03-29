44dd4c364f5b219e1f7548db9f21f29cf3c385bd
Javier Juarez <javier.juarez@gmail.com>
2011-03-29 11:10:38 +0200

    Clean method missing

--- lib/config_context.rb
+++ lib/config_context.rb
@@ -12,8 +12,7 @@ module ConfigContext
   
     if( method.to_s =~ /(.+)=$/ )
     
-      config_key          = method.to_s.delete( '=' ).to_sym
-      @config[config_key] = (arguments.length == 1) ? arguments[0] : arguments
+      @config[method.to_s.delete( '=' ).to_sym] = (arguments.length == 1) ? arguments[0] : arguments
     elsif( method.to_s =~ /(.+)\?$/ )
       
       @config.has_key?( method.to_s.delete( '?' ).to_sym )
@@ -23,25 +22,39 @@ module ConfigContext
     end
   end
 
-  def configure; yield self; end
-
-  def []( key ) return @config[key]; end
-
-  def[]=( key, value ) @config[key] = value; end
-
-  def all; @config; end
-  
-  def keys; @config.keys; end
-
-
   def load( config_file, options = { :allow_collisions => true } )
     
     if( options[:allow_collisions] )
       @config.merge!( YAML.load_file( config_file ) )
     else
-      @config.merge!( YAML.load_file( config_file ) ) { |key, original_value, new_value| original_value }
+      @config.merge!( YAML.load_file( config_file ) ) { |k, ov, nv| ov }
     end
   rescue Exception => e
     raise ConfigContext::Error.new( e.message )
+  end
+
+  def configure()
+    
+    yield self
+  end
+
+  def []( key ) 
+    
+    @config[key]
+  end
+
+  def []=( key, value ) 
+    
+    @config[key] = value
+  end
+
+  def all()
+    
+    @config
+  end
+  
+  def keys
+    
+    @config.keys
   end
 end
