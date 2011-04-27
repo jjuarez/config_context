993f0c8b0308494926cddf4c8535837fd3b18997
Javier Juarez <javier.juarez@gmail.com>
2011-04-27 16:15:50 +0200

    Without submodule, and version information progress

--- lib/version.rb
+++ lib/version.rb
@@ -1,12 +1,11 @@
-module ConfigContext
-  module Version
-    INFO = {
-      :major =>0,
-      :minor =>3,
-      :patch =>2
-    }
+module Version
+  INFO = {
+    :major =>0,
+    :minor =>3,
+    :patch =>3
+  }
 
-    NAME    = 'config_context'
-    VERSION = INFO.values.join( '.' )
-  end
+  NAME    = 'config_context'
+  VERSION = [INFO[:major], INFO[:minor], INFO[:patch]].join( '.' )
 end
+  
