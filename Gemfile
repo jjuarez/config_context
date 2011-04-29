abd44b8aaa872344ac8f41eb7c40a5d2dff324b1
Javier Juarez <javier.juarez@gmail.com>
2011-04-29 14:20:00 +0200

    Updating the tests

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+group :development do
+
+  gem 'jeweler'
+end
+
+group :testing do
+  
+  gem 'shoulda'
+  gem 'rcov'
+  gem 'ZenTest'
+end
--- test/unit/test_config_context.rb
+++ test/unit/test_config_context.rb
@@ -11,24 +11,17 @@ class TestConfigContext < Test::Unit::TestCase
 
   context "A ConfigContext" do
 
-    TEST_SYMBOL      = 'symbol'
-    TEST_LIST        = [1, 2, 3]
-    TEST_HASH        = { :a=>1, :b=>2, :c=>3 }
-    TEST_STRING      = 'string'
-    TEST_OTHERSYMBOL = "othersymbol"
-    
-    
+    TEST_SYMBOL = 'symbol value'
+    TEST_LIST   = [1, 2, 3]
+    TEST_HASH   = { :a=>1, :b=>2, :c=>3 }
+     
     setup do
 
       ConfigContext.configure do |config|
-
         config.mysymbol = TEST_SYMBOL
         config.mylist   = TEST_LIST
         config.myhash   = TEST_HASH
       end
-
-      ConfigContext[TEST_STRING]  = TEST_STRING
-      ConfigContext[:othersymbol] = TEST_OTHERSYMBOL
     end
     
     should "configure properties" do
@@ -36,9 +29,6 @@ class TestConfigContext < Test::Unit::TestCase
       assert_equal( ConfigContext.mysymbol, TEST_SYMBOL )
       assert_equal( ConfigContext.mylist, TEST_LIST )
       assert_equal( ConfigContext.myhash, TEST_HASH )
-      assert_equal( ConfigContext[TEST_STRING], TEST_STRING )
-      assert_equal( ConfigContext.othersymbol, TEST_OTHERSYMBOL )
-      assert_equal( ConfigContext[:othersymbol], TEST_OTHERSYMBOL )
     end
 
     should "check the presence of some properties" do
@@ -46,9 +36,6 @@ class TestConfigContext < Test::Unit::TestCase
       assert( ConfigContext.mysymbol? )
       assert( ConfigContext.mylist? )
       assert( ConfigContext.myhash? )
-      assert( ConfigContext.othersymbol? )
-
-      assert( !ConfigContext.string? ) # Pay attention to this behaviour!!!
 
       assert( !ConfigContext.thisdonotexist? )
     end
@@ -56,53 +43,29 @@ class TestConfigContext < Test::Unit::TestCase
     should "retrive all properties" do
      
       assert_equal( ConfigContext.all, { 
-        :mysymbol    =>TEST_SYMBOL, 
-        :mylist      =>TEST_LIST, 
-        :myhash      =>TEST_HASH, 
-        'string'     =>TEST_STRING,
-        :othersymbol =>TEST_OTHERSYMBOL
+        :mysymbol =>TEST_SYMBOL, 
+        :mylist   =>TEST_LIST, 
+        :myhash   =>TEST_HASH
       } )
     end
     
     should "retrieve all property keys" do
 
-      [ :mysymbol, :mylist, :myhash, 'string', :othersymbol ].each do |key|
+      [ :mysymbol, :mylist, :myhash ].each do |key|
       
-        assert( ConfigContext.keys.include?( key ) )
+        assert( ConfigContext.all.keys.include?( key ) )
       end
     end
 
-    should "load a Yaml file without keys collisions" do
+    should "load a Yaml file" do
           
+      assert_raises( ConfigContext::Error ) { ConfigContext.load( "very_bad_file.yml" ) }
       ConfigContext.load( File.join( File.dirname( __FILE__ ), %w[ .. fixtures test.yml] ) )    
       assert_equal( ConfigContext.all, { 
         :mysymbol    =>TEST_SYMBOL, 
         :mylist      =>TEST_LIST, 
-        :myhash      =>TEST_HASH, 
-        'string'     =>TEST_STRING,
-        :othersymbol =>TEST_OTHERSYMBOL
+        :myhash      =>TEST_HASH 
       } )
-    end
-        
-    should "load a Yaml file with key collisions" do
-    
-      original_value = "The Original value is here"
-      
-      ConfigContext.configure { |config| config.mysymbol = original_value }
-
-      ConfigContext.load( File.join( File.dirname( __FILE__ ), %w[ .. fixtures test.yml] ), :allow_collisions=>false )    
-      assert_equal( ConfigContext.all, { 
-        :mysymbol    =>original_value, 
-        :mylist      =>TEST_LIST, 
-        :myhash      =>TEST_HASH, 
-        'string'     =>TEST_STRING,
-        :othersymbol =>TEST_OTHERSYMBOL
-      } )
-    end
-    
-    should "load a Yaml file with error" do
-        
-      assert_raises( ConfigContext::Error ) { ConfigContext.load( "very bad file.yml" ) }
     end
     
     should "update properties" do
@@ -111,13 +74,15 @@ class TestConfigContext < Test::Unit::TestCase
       ConfigContext.mysymbol = "A"
       assert_equal( ConfigContext.mysymbol, "A" )
 
+      new_test_list = [4,5,6]
       assert_equal( ConfigContext.mylist, TEST_LIST )
-      ConfigContext.mylist = [4,5,6]
-      assert_equal( ConfigContext.mylist, [4,5,6] )
+      ConfigContext.mylist = new_test_list
+      assert_equal( ConfigContext.mylist, new_test_list )
 
+      new_hash = { :d=>4, :e=>5, :f=>6 }
       assert_equal( ConfigContext.myhash, TEST_HASH )
-      ConfigContext.myhash= { :d=>4, :e=>5, :f=>6 }
-      assert_equal( ConfigContext.myhash, { :d=>4, :e=>5, :f=>6 } )
+      ConfigContext.myhash = new_hash
+      assert_equal( ConfigContext.myhash, new_hash )
     end
   end
 end
