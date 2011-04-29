a057894735e7f39f982afe8cf1adef9271f02fb5
Javier Juarez <javier.juarez@gmail.com>
2011-04-29 14:19:42 +0200

    Progress the version information

--- lib/version.rb
+++ lib/version.rb
@@ -2,10 +2,9 @@ module Version
   INFO = {
     :major =>0,
     :minor =>3,
-    :patch =>4
+    :patch =>5
   }
 
   NAME    = 'config_context'
   VERSION = [INFO[:major], INFO[:minor], INFO[:patch]].join( '.' )
 end
-  
